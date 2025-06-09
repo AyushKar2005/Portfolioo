3410a24635b1c14b69182c7a4d5c1f48887b6a91
Ayush <Ayushkar2005@gmail.com>
2025-06-09 22:12:13 +0530

    Fix: corrected import path for ContactExperience

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -2,7 +2,8 @@ import { useRef, useState } from "react";
 import emailjs from '@emailjs/browser'
 
 import TitleHeader from "../components/TitleHeader";
-import ContactExperience from "../components/models/contact/ContactExperience";
+import ContactExperience from "../components/Models/contact/ContactExperience";
+
 import {browser} from "globals";
 
 const Contact = () => {
